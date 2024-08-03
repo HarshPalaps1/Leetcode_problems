c7893e0c77e3285eecaebf10e5704259e3cec616
Harsh pal <86770957+HarshPalaps1@users.noreply.github.com>
2024-08-03 16:57:26 +0530

    Time: 3738 ms (10.25%), Space: 12.5 MB (37.82%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i<=j:
+                    pass
+                else:
+                    if nums[i]+nums[j]==target:    
+                        return [i,j]
+
+
+
+                
+        
