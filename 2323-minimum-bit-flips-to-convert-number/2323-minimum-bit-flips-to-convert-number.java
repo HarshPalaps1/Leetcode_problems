46061a2322bede19c7ef500ce564896425e99064
Harsh pal <86770957+HarshPalaps1@users.noreply.github.com>
2024-11-16 19:10:32 +0530

    Time: 0 ms (100%), Space: 40.6 MB (28.04%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
@@ -1,28 +1,5 @@
 class Solution {
-
-    public static int[] binary(int num ){
-        int[] bi_num=new int[30]; 
-        int position =0;
-    
-        while(num>0){
-           bi_num[position]=num%2;
-           num=num/2;
-           position++; 
-                   }
-        
-        
-        return bi_num;
-    }
-
     public int minBitFlips(int start, int goal) {
-      int[] Start=binary(start);
-      int[] Goal=binary(goal);
-      int count=0;
-      for(int i=0;i<Start.length;i++){
-      if( Start[i]!=Goal[i]){
-            count++;
-      }
-     
-      
-    } return count;}
+        return Integer.bitCount(start ^ goal);
+    }
 }
